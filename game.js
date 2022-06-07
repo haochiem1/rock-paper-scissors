d1385d6a1bfbe2424b09ed9e73d033e1f60caada
Hao Chiem <haochiem1@gmail.com>
2022-06-07 18:06:53 -0400

    Added user input and validation, round play that handles win conditions, and game finction that plays 5 round

--- game.js
+++ game.js
@@ -4,15 +4,101 @@ function computerPlay(){
     switch(x)
     {
         case 0:
-            return "Rock";
+            return "ROCK";
         case 1:
-            return "Paper";
+            return "PAPER";
         case 2:
-            return "Scissors";
+            return "SCISSORS";
     }
 
     return "Error. Try Again!"
 }
 
-let test = computerPlay();
-console.log(test);
+function validateInput(playerSelection)
+{
+    let string = playerSelection.toUpperCase();
+    if (string == "ROCK" || string == "PAPER" || string == "SCISSORS")
+    {
+        return string;
+    }
+
+    return "ERROR";
+}
+
+function playRound(playerSelection, computerSelection) {
+    var resultString;
+    var winner;
+    var loser;
+
+    if(playerSelection == computerSelection)
+    {
+        return "You Tied!";
+    }
+
+    switch (playerSelection)
+    {
+        case "ROCK":
+            if(computerSelection == "PAPER")
+            {
+                resultString = "You Lose!";
+                winner = computerSelection;
+                loser = playerSelection;
+            }
+            else
+            {
+                resultString = "You Win!";
+                winner = playerSelection;
+                loser = computerSelection;
+            }
+            break;
+        case "PAPER":
+            if(computerSelection == "SCISSORS")
+            {
+                resultString = "You Lose!";
+                winner = computerSelection;
+                loser = playerSelection;
+            }
+            else
+            {
+                resultString = "You Win!";
+                winner = playerSelection;
+                loser = computerSelection;
+            }
+            break;
+        case "SCISSORS":
+            if(computerSelection == "ROCK")
+            {
+                resultString = "You Lose!";
+                winner = computerSelection;
+                loser = playerSelection;
+            }
+            else
+            {
+                resultString = "You Win!";
+                winner = playerSelection;
+                loser = computerSelection;
+            }
+            break;
+    }
+
+    return resultString + ' ' + winner + ' beats ' + loser;
+}
+function game(){
+    for (let i = 0; i < 5; i++)
+    {
+        var playerSelection = prompt("Enter your choice! Rock, Paper, or Scissors?")
+        playerSelection = validateInput(playerSelection);
+
+        if(playerSelection == "ERROR")
+        {
+            console.log("Invalid Input! Try Again");
+            i--;
+            continue;
+        }
+
+        let round = playRound(playerSelection, computerPlay());
+        console.log(round);
+    }
+}
+
+game();
